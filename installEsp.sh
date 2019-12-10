85b16c58a304faddeee419a074bf12d56f304ec3
Santiago Corrao <santiagocorrao96@outlook.com>
2019-12-10 12:12:33 -0300

    first commit

---
+++ installEsp.sh
@@ -0,0 +1,10 @@
+cd hardware
+sudo mkdir esp8266com
+cd esp8266com
+sudo git clone https://github.com/esp8266/Arduino.git esp8266
+sudo git submodule update --init
+cd esp8266/tools
+sudo python3 get.py
+cd ..
+sudo git status
+sudo git pull
