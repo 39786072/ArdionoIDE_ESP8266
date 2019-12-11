ae4970b41c20f93311ab359f7353ff0bfbacfc5c
Santiago Corrao <santiagocorrao96@outlook.com>
2019-12-11 19:09:39 -0300

    fix installEsp error

--- installEsp.sh
+++ installEsp.sh
@@ -2,8 +2,9 @@ cd hardware
 sudo mkdir esp8266com
 cd esp8266com
 sudo git clone https://github.com/esp8266/Arduino.git esp8266
+cd esp8266
 sudo git submodule update --init
-cd esp8266/tools
+cd tools
 sudo python3 get.py
 cd ..
 sudo git status
